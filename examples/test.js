ee70bf73800e35f76ef48adb3924abd910eb998d
Gordon Martin <gordon.hugh.martin@gmail.com>
2018-09-10 18:02:20 +0100

    Moved examples to ssb-bluetooth, added index file.

--- examples/test.js
+++
@@ -1,6 +0,0 @@
-var bluetoothManager = require('../bluetooth-manager')();
-var pull = require('pull-stream');
-
-bluetoothManager.listenForIncomingConnections( (err, res) => {
-  pull(res.source, pull.drain((data) => console.log ("Got from client: " + data)));
-} );
